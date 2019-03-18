846b55b40d0f14ef4791d1c6508eabf5b43a0fa8
Fredrik <s185130@student.dtud.k>
2019-03-18 15:06:02 +0100

    Dummy class with user and batch info

---
+++ CDIO2reference/src/Data/Dummy.java
@@ -0,0 +1,28 @@
+package Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dummy implements IDatabaseAccess {
+
+    Map<Integer,String> user;
+    Map<Integer,String> batch;
+
+    Dummy(){
+        user = new HashMap<>();
+        user.put(12,"Anders And");
+        batch = new HashMap<>();
+        batch.put(1234,"Salt");
+    }
+
+    @Override
+    public String getOperator(int userID) {
+        return user.get(userID);
+    }
+
+    @Override
+    public String getMaterial(int materialID) {
+        return batch.get(materialID);
+    }
+}
